20eae4ab2281d2df64b4f03ba03aa9e0b5b80dc1
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2023-10-04 00:27:54 +0000

    questão 3

--- AtividadeVetores2/Questao3/main.c
+++ AtividadeVetores2/Questao3/main.c
@@ -3,6 +3,29 @@
 
 int main()
 {
+    int vetor1[10], vetor2[10], vetor3[20];
+    for (int i = 0, cont = 0; i < 10; i++)
+    {
+        printf("Digite um valor para ser armazenado no vetor1: ");
+        scanf("%d", &vetor1[i]);
+        vetor3[i+cont] = vetor1[i];
+        cont++;
+    }
+    printf("\n");
+    for (int i = 0, cont = 0; i < 10; i++)
+    {
+        cont++;
+        printf("Digite um valor para ser armazenado no vetor2: ");
+        scanf("%d", &vetor2[i]);
+        vetor3[i+cont] = vetor2[i];
+    }
+  
+
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%d ", vetor3[i]);
+    }
     
     return 0;
 }
