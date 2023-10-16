786a087330594d949fb8bea56ba69a7c37523762
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2023-10-16 10:52:05 +0000

    terminadas

--- AtividadeVetores2/Questao9/main.c
+++ AtividadeVetores2/Questao9/main.c
@@ -1,20 +1,39 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-    int matriz[6][6], quant = 0;
-    for (int x = 0; x < 6; x++)
-    {
-        for (int y = 0; y < 6; y++)
-        {
-            printf("Digite o valor da posicao [%d][%d]: ", x, y);
-            scanf("%d", &matriz[x][y]);
-            if (matriz[x][y] > 10)
-            {
-                quant++;
+int main()
+{
+    char gabarito[10], respostas[20][10];
+    int resultado[20] = {0};
+
+    printf("Digite as respostas do gabarito (a, b, c ou d):\n");
+    for (int i = 0; i < 10; i++){
+        printf("Questao %d: ", i+1);
+        scanf(" %c", &gabarito[i]);
+    }
+
+    for (int i = 0; i < 20; i++){
+        printf("Digite as respostas do aluno %d (a, b, c ou d):\n", i+1);
+        for (int j = 0; j < 10; j++){
+            printf("Questao %d: ", j+1);
+            scanf(" %c", &respostas[i][j]);
+            while (respostas[i][j] != 'a' && respostas[i][j] != 'b' && respostas[i][j] != 'c' && respostas[i][j] != 'd'){
+                printf("Resposta invalida. Digite novamente (a, b, c ou d): ");
+                scanf(" %c", &respostas[i][j]);
             }
         }
     }
-    printf("A quantidade de números maiores que 10 é: %d", quant);
+
+    for (int i = 0; i < 20; i++){
+        for (int j = 0; j < 10; j++){
+            if (respostas[i][j] == gabarito[j]){
+                resultado[i]++;
+            }
+        }
+    }
+    printf("Vetor resultado:\n");
+    for (int i = 0; i < 20; i++){
+        printf("Aluno %d: %d pontos\n", i+1, resultado[i]);
+    }
+
     return 0;
 }
