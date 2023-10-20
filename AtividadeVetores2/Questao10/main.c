a5a96a8d23159a5af656239a5d7f88c1a9f9c19c
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2023-10-20 16:49:50 +0000

    10

--- AtividadeVetores2/Questao10/main.c
+++ AtividadeVetores2/Questao10/main.c
@@ -1,23 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-    char matriz[20][10], gabaritou[10];
-    for (int x = 0; x < 10; x++)
-    {
-        printf("Digite o gabarito da %d° questao: ", x+1);
-        scanf("%d", &gabaritou[x]);
+int main(void) {
+  char certo[10], respostas[20][10], pontos=0;
+  int nota[20];
+  for(int l=0; l<10; l++){
+    printf("Digite a resposta correta da questao %d: ", l+1);
+    scanf("%s", &certo[l]);
+  }
+  for(int l=0; l<20; l++){
+    printf("\n");
+    for(int c=0; c<10; c++){
+      printf("Digite a resposta do aluno %d na questao %d: ", l+1,c+1);
+      scanf("%s", &respostas[l][c]);
     }
-
-    for (int x = 0; x < 20; x++)
-    {
-        printf("\nDigite o nome do Aluno: ");
-        scanf("%s", &matriz[x][0]);
-        for (int y = 0; y < 10; y++)
-        {
-            printf("Digite a %d° questao: ", y+1);
-            scanf("%d", &matriz[x][y]);
-        }
-    } 
-    return 0;
+  }
+  for(int l=0; l<20; l++){
+    for(int c=0; c<10; c++){
+      if(respostas[l][c]==certo[c]){
+        pontos++;
+      }
+    }
+    nota[l]=pontos;
+    pontos=0;
+    printf("\nO aluno %d tirou nota %d",l+1,nota[l]);
+  }
+  return 0;
 }
