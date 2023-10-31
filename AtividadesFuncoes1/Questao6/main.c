720a4aacee233927a2371c17893f873a834caea0
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2023-10-31 19:07:58 +0000

    fim das atividades

---
+++ AtividadesFuncoes1/Questao6/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int x;
+bool boleanopositivo, boleanopar;
+
+int main () {
+    printf("Digite um número: ");
+    scanf("%d", &x);
+    boleanopositivo = sinal(x);
+    boleanopar = par(x);
+    if (boleanopar){
+        printf("\n%d é par.\n", x);
+    }
+    else{
+        printf("\n%d é ímpar.\n", x);
+    }
+    if (boleanopositivo){
+        printf("\n%d é positivo.\n", x);
+    }
+    else{
+        printf("\n%d é negativo.\n", x);
+    }
+    return 0;
+}
+
+bool sinal (int x){
+    if (x >= 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool par (int x){
+    if (x % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
